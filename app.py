d8ab16a5448d8b807928affdf8d90e78c11feda7
Siddharth Jha <sid@sidjha.com>
2016-08-25 18:57:09 -0400

    Partially implemented find nearby circles

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, json, g
 import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 app = Flask(__name__)
 APP_SETTINGS="config.DevConfig"
@@ -9,6 +10,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Circle, Seed, User
+
+NEARBY_CIRCLES_THRESHOLD_DIST = 2000.0 # in meters. 
 
 @app.route("/")
 def index():
@@ -22,31 +25,26 @@ def api_circle():
     """
 
     # Parse arguments, extract latitude and longitude
-    lat = request.args.get("lat", '')
-    lng = request.args.get('lng', '')
+    lat = float(request.args.get("lat", ''))
+    lng = float(request.args.get('lng', ''))
 
-    # TODO: Query database for circles with nearby centres
-    
-    # Check list to see whether point is inside existing circle
+    if lat and lng:
+        # convert current coordinates to a Geography object
+        point = func.ST_GeogFromText('SRID=4326;POINT(%f %f)' % (lng, lat)) 
 
-    # for each point in list
-    # 1. convert point to radians
-    # 2. calculate geodesic
-    # a = (math.sin(lat)*math.sin(p_lat)+math.cos(lat)*math.cos(p_lat)*math.cos(lng - p_lng))
-    # dist = (math.acos(a))*6371
-    # 3. if distance <= radius, point is inside. convert to JSON and return.
+        # call PostGIS function ST_DWithin to find 5 nearby points
+        nearby = db.session.query(Circle).filter(func.ST_DWithin(point, Circle.point, NEARBY_CIRCLES_THRESHOLD_DIST)).limit(5)
 
-    # convert 5 items from list to JSON-formatted list
+        # TODO: order nearby circles by distance, in ascending order
 
+        if nearby.count() > 0:
+            # TODO: convert list to JSON-formatted list
+            return "Found %d nearby circles!" % nearby.count()
+        else:
+            return "Sorry, no circles found nearby."
 
 def is_in_circle(latitude, longitude):
     # problem: need an algorithm that determines whether these coords are within a defined circle
-    # solution: 
-    #  the database needs to store the center coordinates of every circle, and the radius.
-    #  so each record in the Circles db will be a (coordinates, radius) pair
-    #  now we have available as input coordinates and we need to determine whether these coordinates
-    #  are equal to any one circle's all possible interior coordinates. that's a very time-consuming
-    #  computation as there will be an infinite # of coordinates.
 
     # solution 2:
     # take the input coordinates A.
@@ -56,7 +54,23 @@ def is_in_circle(latitude, longitude):
     #   that's the circle A belongs to.
     #   return.
     # if no match, then there are no circles closeby.
+
+    # TODO: implementation of solution 2
+    # Check list to see whether point is inside existing circle
+    # for each point in list
+    # 1. convert point to radians
+    # 2. calculate geodesic
+    # a = (math.sin(lat)*math.sin(p_lat)+math.cos(lat)*math.cos(p_lat)*math.cos(lng - p_lng))
+    # dist = (math.acos(a))*6371
+    # 3. if distance <= radius, point is inside. convert to JSON and return.
     pass
+
+def populate_GIS():
+    circle1 = Circle(center_lat=37.332376, center_lng=-122.030754, point='POINT(-122.030754 37.332376)', radius='150', name='infinite loop', city='Cupertino')
+    circle2 = Circle(center_lat=37.414172, center_lng=-122.038672, point='POINT(-122.038672 37.414172)', radius='200', name='airbase', city='Cupertino')
+    circle4 = Circle(center_lat=37.777025, center_lng=-122.416583, point='POINT(-122.416583 37.777025)', radius='200', name='twitter hq', city='San Francisco')
+    circle3 = Circle(center_lat=40.748636, center_lng=-73.985654, point='POINT(-73.985654 40.748636)', radius='100', name='empire state', city='Empire State Bldg')
+
 
 if __name__ == "__main__":
     app.run()
